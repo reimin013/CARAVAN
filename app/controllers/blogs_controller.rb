502aec2de1f84a2282a3d29c4de05a8e09e84c71
reimi <utatatan.013@gmail.com>
2020-03-30 16:57:55 +0900

    [Update] 詳細ページを追加、投稿内容が表示されないエラーを解消

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,9 +1,10 @@
 class BlogsController < ApplicationController
-  def index
-  	@blogs = Blog.all
+  def show
+    @blog = Blog.find(params[:id])
   end
 
-  def show
+  def index
+    @blogs = Blog.all
   end
 
   def new
@@ -11,9 +12,9 @@ class BlogsController < ApplicationController
   end
 
   def create
-  	blog = Blog.new
+  	blog = Blog.new(blog_params)
   	blog.save
-  	redirect_to blogs_path
+  	redirect_to blog_path(blog.id)
   end
 
   def edit
@@ -21,6 +22,6 @@ class BlogsController < ApplicationController
 
   private
   def blog_params
-  	params.require(:blog).permit(:title, :category, :body)
+  	params.require(:blog).permit(:title,:category,:body)
   end
 end
